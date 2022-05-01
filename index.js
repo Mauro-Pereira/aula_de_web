383c43bb801ed0ba225f6c37b4c77a831c85aa4f
Santos <mauro.santos@vetta.digital>
2022-05-01 15:50:56 -0300

    atualizando o arquivo index.js

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const route = express.Router();
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
-//mongodb+srv://aulaWeb:<password>@cluster0.bzv1z.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
 app.get('/login', (req,res) =>{
     res.render("login");
 })
